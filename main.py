c5db5722519ff59c3aa56796e9ccfe637de370af
Shushanna <shushanna.xachatryan1998@gmail.com>
2026-04-13 10:21:05 +0400

    right solution

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ def even_numbers(listn):
         if i % 2 == 0:
             even_list.append(i)
     return even_list 
-listn = [int(input()) for _ in range(5)]
+listn = [1, 4, 5, 6, 8]
 print(even_numbers(listn))
